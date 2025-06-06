97f440b317a243cbef1ace71ef61bf7096f2c870
J-Henriksson <henriksson.joel2@gmail.com>
2025-06-06 13:36:21 +0200

    small fix

--- Algorithms/fibonacci_dp.py
+++ Algorithms/fibonacci_dp.py
@@ -1,13 +1,13 @@
 class DpFibonacci:
     
-    def __init__(self, mem=[1,1]):
+    def __init__(self, mem=[0,1]):
         self.mem = mem
     
     def dynamic_fibonacci(self, n):
         if n > len(self.mem):
-            for i in range(len(self.mem), n):
+            for i in range(len(self.mem), n + 1):
                 self.mem.append(self.mem[i - 1] + self.mem[i - 2])
-        return self.mem[n - 1]
+        return self.mem[n]
 
 
 if __name__ == "__main__":
